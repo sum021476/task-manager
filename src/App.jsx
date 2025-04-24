de5aa2c2c41ccbd6297166826af321ce90349842
Sumanth Vadlamani <sumanthvadlamani@gmail.com>
2025-04-24 20:54:18 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RouterProvider} from 'react-router-dom'
 import { routes } from './components/routes/Routes'
 
 const App = () => {
-  return <RouterProvider router={routes}></RouterProvider>
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/db.json')
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  return (<>
+  <h1>Data from db.json</h1>
+  <pre>{JSON.stringify(data, null, 2)}</pre>
+  <RouterProvider router={routes}></RouterProvider>
+  </>
+  )
 }
 
 export default App
